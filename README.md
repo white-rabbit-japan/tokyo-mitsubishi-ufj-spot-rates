859af95aa4a61c37ec40313d1771e4d69452e05a
maxhodges <max@whiterabbitjapan.com>
2015-03-29 23:07:44 +0900

    Add content to readme. Add github link in package describge

---
+++ README.md
@@ -0,0 +1,4 @@
+# tokyo-mitsubishi-ufj-spot-rates
+Meteor package - returns forex spot rates from Bank of Tokyo Mitsubishi UFJ.
+
+USD only for now.
--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
   version: '0.0.1',
   summary: 'Returns forex spot rates from Bank of Tokyo Mitsubishi UFJ. USD only for now.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/white-rabbit-japan/tokyo-mitsubishi-ufj-spot-rates.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'

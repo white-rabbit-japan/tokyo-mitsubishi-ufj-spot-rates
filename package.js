ad5994d698977241a1d403ea9e1b3f3a4a2e32a0
Jeremy Hall <jeremy@whiterabbitjapan.com>
2015-04-22 18:23:25 +0900

    Change to updated data format.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'whiterabbit:tokyo-mitsubishi-ufj-spot-rates',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'Returns forex spot rates from Bank of Tokyo Mitsubishi UFJ. USD only for now.',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/white-rabbit-japan/tokyo-mitsubishi-ufj-spot-rates.git',
--- tokyo-mitsubishi-ufj-spot-rates.js
+++ tokyo-mitsubishi-ufj-spot-rates.js
@@ -6,37 +6,35 @@ Meteor.methods({
   mufgRates: function() {
     this.unblock();
     var result = HTTP.call("GET", "http://www.bk.mufg.jp/gdocs/kinri/list_j/kinri/spot_rate.csv");
-    if (result.statusCode === 200) {
-      console.log("success");
-      console.log(result.content);
 
-      var forex = {};
-      forex.base = 'JPY';
+    check(result, Match.Where(function(result) {
+      return result.statusCode === 200;
+    }));
 
-      //extract date time
-      var re = /(\d{4}\/\d{2}\/\d{2})\s\d{2}:\d{2}/;
-      var pos = result.content.search(re);
-      forex.updatedAt = new Date(result.content.slice(pos, pos + 16));
-      forex.usdRates = [];
+    //extract date time
+    var re = /(\d{4}\/\d{2}\/\d{2})\s\d{2}:\d{2}/;
+    var pos = result.content.search(re);
+    var updatedAt = new Date(result.content.slice(pos, pos + 16));
 
-      var res = result.content.slice(result.content.indexOf('USD') - 1);
-      res = res.slice(0, res.indexOf('\n'));
-      var rates = res.split(',');
+    var res = result.content.slice(result.content.indexOf('USD') - 1);
+    res = res.slice(0, res.indexOf('\n'));
+    var rates = res.split(',');
 
-      forex.usdRates.push({
-        'ttBuy': _extractFloat(rates[4]) * 100,
-        'cashBuy': _extractFloat(rates[7]) * 100,
-        'cashSell': _extractFloat(rates[3]) * 100,
-        'atSight': _extractFloat(rates[5]) * 100,
-        'ttSell': _extractFloat(rates[1]) * 100,
-      });
+    var forex = {
+      base: 'JPY',
+      updatedAt: updatedAt,
+      currencies: [{
+        currency: 'USD',
+        rates: {
+          'ttBuy': _extractFloat(rates[4]) * 100,
+          'cashBuy': _extractFloat(rates[7]) * 100,
+          'cashSell': _extractFloat(rates[3]) * 100,
+          'atSight': _extractFloat(rates[5]) * 100,
+          'ttSell': _extractFloat(rates[1]) * 100,
+        },
+      }],
+    };
 
-      console.log(forex);
-
-      return forex;
-    } else {
-      console.log('fail');
-      return false;
-    }
+    return forex;
   }
 });
